4026f5414824f0a8738a28284fdd6aefe23968c3
João Gabriel Trombeta <joaog.trombeta@gmail.com>
2022-02-22 09:28:44 -0300

    Remove default message from NotEquals

--- PingPong/Models/Validators/NotEquals.cs
+++ PingPong/Models/Validators/NotEquals.cs
@@ -5,7 +5,6 @@ namespace PingPong.Models.Validators
     public class NotEquals : ValidationAttribute
     {
         private readonly string[] _propertyNames;
-        private readonly string _defaultMessageTemplate = "{0} cannot be the same as {1}";
 
         public NotEquals(params string[] PropertyNames)
         {
@@ -14,24 +13,18 @@ namespace PingPong.Models.Validators
 
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
-            var errorMessage = !String.IsNullOrEmpty(ErrorMessage) ? ErrorMessage
-                : String.Format(this._defaultMessageTemplate, validationContext.MemberName, string.Join(", ", _propertyNames));
-
             foreach(var propertyName in _propertyNames)
             {
                 var comparisonProperty = validationContext.ObjectType.GetProperty(propertyName);
                 if (comparisonProperty == null)
                 {
-                    throw new ArgumentException(String.Format("Property {0} not found", propertyName));
+                    throw new ArgumentException(string.Format("Property {0} not found", propertyName));
                 }
 
                 var comparisonValue = comparisonProperty.GetValue(validationContext.ObjectInstance, null);
-                if (value == null && comparisonValue == null)
+                if ((value == null && comparisonValue == null) || (value != null && value.Equals(comparisonValue)))
                 {
-                    return new ValidationResult(errorMessage);
-                } else if (value != null && value.Equals(comparisonValue))
-                {
-                    return new ValidationResult(errorMessage);
+                    return new ValidationResult(ErrorMessage);
                 }
             }
 
